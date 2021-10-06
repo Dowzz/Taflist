6391c8e90a48759cdde2d54e4f108620db235166
Dowzz <michel.barny@outlook.fr>
2021-10-06 14:16:29 +0200

    test en cours
    Signed-off-by: Dowzz <michel.barny@outlook.fr>

--- admin.php
+++ admin.php
@@ -21,11 +21,32 @@
     if (!isset($_SESSION['userid'])) {
         header('Location: login.php');
     }
+    //admin role = 1
+    //waiting role = 2
+    //waiting role consultant = 3
+    //consultant role = 4
+    //employeur role = 5
+    //candidat role = 6
     ?>
 
-    <h1>Welcome
-
+    <h1>Welcome <?php echo $_SESSION['name'] ?>
     </h1>
+    <?php
+    if ($_SESSION['role'] == 1) {
+    ?><span>Administrateur</span>
+    <?php
+    } else if ($_SESSION['role'] == 4) {
+    ?><span>Consultant</span>
+    <?php
+    } else if ($_SESSION['role'] == 5) {
+    ?><span>Employeur</span>
+    <?php
+    } else if ($_SESSION['role'] == 6) {
+    ?><span>Candidat</span>
+    <?php
+    }
+    ?>
+
 
     <br><br>
     <?php include('footer.php'); ?>
