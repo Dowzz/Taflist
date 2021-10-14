3bbf7e910f5b8e0fcc292b81f730c9d16e367de5
Dowzz <michel.barny@outlook.fr>
2021-10-14 09:31:26 +0200

    config bdd
    Signed-off-by: Dowzz <michel.barny@outlook.fr>

--- connect.php
+++ connect.php
@@ -1,5 +1,7 @@
 <?php
-$con= mysqli_connect('localhost', 'root', '', 'backend');
-if(!$con){
+$con = mysqli_connect('eu-cdbr-west-01.cleardb.com', 'b5f4671c90ec0c', '06fdfc84', 'heroku_008dd6fd6858b5c');
+if (!$con) {
     die('Can`\'t connect to database');
 }
+
+//mysql://b5f4671c90ec0c:06fdfc84@eu-cdbr-west-01.cleardb.com/heroku_008dd6fd6858b5c?reconnect=true
